7e2e4e44f7771ca8d2f74e1338a7c42e0e2a3e84
Tomasz Pakuła <forest10pl@gmail.com>
2026-01-26 00:21:26 +0100

    update

--- input-daemon/__main__.py
+++ input-daemon/__main__.py
@@ -10,45 +10,51 @@ from mapping import cec_mapping
 
 print("starting cec-toolbox input daemon\n")
 
-devices = [InputDevice(path) for path in list_devices()]
-cec = None
+def main_loop() -> None:
+    devices = [InputDevice(path) for path in list_devices()]
+    cec = None
 
-for device in devices:
-    if match("DP-[0-9]", device.name):
-        cec = device
+    for device in devices:
+        if match("DP-[0-9]", device.name):
+            cec = device
 
-if not cec:
-    print("No CEC input devices found!")
-    exit(0)
+    if not cec:
+        print("No CEC input devices found!")
+        return
 
-cec.grab()
-cap = cec.capabilities(verbose=False)
-for code in sum(cec_mapping.values(), []):
-    if code not in cap[e.EV_KEY]:
-        cap[e.EV_KEY].append(code)
+    cec.grab()
+    cap = cec.capabilities(verbose=False)
+    for code in sum(cec_mapping.values(), []):
+        if code not in cap[e.EV_KEY]:
+            cap[e.EV_KEY].append(code)
 
-if e.EV_SYN in cap:
-    cap.pop(e.EV_SYN)
+    if e.EV_SYN in cap:
+        cap.pop(e.EV_SYN)
 
-virt = UInput(events=cap, name='CEC-toolbox Virtual Input')
+    virt = UInput(events=cap, name='CEC-toolbox Virtual Input')
 
-event: InputEvent
-for event in cec.read_loop():
-    if event.type != e.EV_KEY:
-        continue
+    event: InputEvent
+    for event in cec.read_loop():
+        if event.type != e.EV_KEY:
+            continue
 
-    key = KeyEvent(event)
-    new_keys: list[int] = []
+        key = KeyEvent(event)
+        new_keys: list[int] = []
 
-    if key.scancode not in cec_mapping:
-        continue
+        if key.scancode not in cec_mapping:
+            continue
 
-    if key.keystate == 1:
-        print(f"Detected supported key: {key.keycode}")
+        if key.keystate == 1:
+            print(f"Detected supported key: {key.keycode}")
 
-    for code in cec_mapping[key.scancode]:
-        virt.write(e.EV_KEY, code, key.keystate)
-        virt.syn()
-        sleep(0.1)
+        for code in cec_mapping[key.scancode]:
+            virt.write(e.EV_KEY, code, key.keystate)
+            virt.syn()
+            sleep(0.1)
 
-cec.ungrab()
+    cec.ungrab()
+
+
+while True:
+    main_loop()
+    sleep(2)
